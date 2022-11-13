0f16f0b8f462c9a468e2b488cd7bc4e3373037f9
Devvara Rishivian <rishivian@gmail.com>
2022-11-13 18:10:40 +0700

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,87 @@
-import { Navbar } from "flowbite-react";
+import { Disclosure } from "@headlessui/react";
+import { Menu as IconMenu, X as IconX } from "react-feather";
+
+const navigation = [
+  { name: "Beranda", href: "#", current: true },
+  { name: "Produk", href: "#produk", current: false },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 export default function AppNavbar() {
   return (
-    <Navbar className="fixed w-full z-20 top-0 left-0 bg-pink-800">
-      <Navbar.Brand href="/" className="h-10 sm:h-12">
-        <span className="self-center whitespace-nowrap text-xl font-semibold text-white">
-          Nuni Beauty Care
-        </span>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse>
-        <Navbar.Link href="#beranda" className="text-stone-300">
-          Beranda
-        </Navbar.Link>
-        <Navbar.Link href="#product" className="text-stone-300">
-          Product
-        </Navbar.Link>
-      </Navbar.Collapse>
-    </Navbar>
+    <Disclosure
+      as="nav"
+      className="bg-pink-700 fixed w-full top-0 z-10 bg-opacity-30 backdrop-filter backdrop-blur-lg"
+    >
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <IconX className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <IconMenu className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <span className="self-center whitespace-nowrap text-xl font-semibold text-white">
+                    Nuni Beauty Care
+                  </span>
+                </div>
+                <div className="hidden sm:ml-auto sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? "bg-gray-900 text-white"
+                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                          "px-3 py-2 rounded-md text-sm font-medium"
+                        )}
+                        aria-current={item.current ? "page" : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pt-2 pb-3">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current
+                      ? "bg-gray-900 text-white"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "block px-3 py-2 rounded-md text-base font-medium"
+                  )}
+                  aria-current={item.current ? "page" : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   );
 }
