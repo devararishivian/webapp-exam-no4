59ef5bae0eab2901bd9e857bb3b9627895143bcc
Devvara Rishivian <rishivian@gmail.com>
2022-11-13 23:54:13 +0700

    save

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -72,105 +72,22 @@ export default function AppProduct() {
   }
 
   return (
-    <section id="produk" className="pt-36">
+    <section id="produk" className="pt-24 bg-gray-800 min-h-screen">
+      <div class="mx-auto max-w-lg text-center">
+        <h2 class="text-3xl font-bold sm:text-4xl text-white">
+          Produk Nuni Beauty Care
+        </h2>
+
+        <p class="mt-4 text-gray-300">
+          Temukan produk kecantikan terbaik hanya di Nuni Beauty Care. Produk
+          kami sudah pasti asli dan memiliki sertifikasi BPOM.
+        </p>
+      </div>
       <div className="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:items-start">
-          <div className="lg:top-4">
-            <details
-              open={windowSize.innerWidth > 768}
-              className="overflow-hidden rounded border border-gray-200"
-            >
-              <summary className="flex items-center justify-between bg-gray-100 px-5 py-3 lg:hidden">
-                <span className="text-sm font-medium"> Toggle Filters </span>
-                <svg
-                  className="h-5 w-5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              </summary>
-              <form className="border-t border-gray-200 lg:border-t-0">
-                <fieldset>
-                  <legend className="block w-full bg-gray-50 px-5 py-3 text-xs font-medium">
-                    Kategori
-                  </legend>
-                  <div className="space-y-2 px-5 py-6">
-                    <div className="flex items-center">
-                      <input
-                        id="1"
-                        type="checkbox"
-                        name="type[1]"
-                        className="h-5 w-5 rounded border-gray-300"
-                      />
-                      <label htmlFor="1" className="ml-3 text-sm font-medium">
-                        Basic Skin Care
-                      </label>
-                    </div>
-                    <div className="flex items-center">
-                      <input
-                        id="2"
-                        type="checkbox"
-                        name="type[2]"
-                        className="h-5 w-5 rounded border-gray-300"
-                      />
-                      <label htmlFor="2" className="ml-3 text-sm font-medium">
-                        Sensitive Skin
-                      </label>
-                    </div>
-                    <div className="flex items-center">
-                      <input
-                        id="3"
-                        type="checkbox"
-                        name="type[3]"
-                        className="h-5 w-5 rounded border-gray-300"
-                      />
-                      <label htmlFor="3" className="ml-3 text-sm font-medium">
-                        Eye Cream
-                      </label>
-                    </div>
-                    <div className="flex items-center">
-                      <input
-                        id="4"
-                        type="checkbox"
-                        name="type[4]"
-                        className="h-5 w-5 rounded border-gray-300"
-                      />
-                      <label htmlFor="4" className="ml-3 text-sm font-medium">
-                        Body Care
-                      </label>
-                    </div>
-                  </div>
-                </fieldset>
-                <div className="flex justify-between border-t border-gray-200 px-5 py-3">
-                  <button
-                    name="reset"
-                    type="button"
-                    className="rounded text-xs font-medium text-gray-600 underline"
-                  >
-                    Reset Filter
-                  </button>
-                  <button
-                    name="commit"
-                    type="button"
-                    className="rounded bg-green-600 px-5 py-3 text-xs font-medium text-white"
-                  >
-                    Apply Filters
-                  </button>
-                </div>
-              </form>
-            </details>
-          </div>
-          <div className="lg:col-span-3">
+          <div className="lg:col-span-4">
             <div className="flex items-center justify-between">
-              <p className="text-sm text-gray-500">
+              <p className="text-sm text-gray-300">
                 <span className="hidden sm:inline"> Menampilkan </span>
                 {products.length} dari {products.length} Produk
               </p>
@@ -197,11 +114,11 @@ export default function AppProduct() {
                 </div>
               </div>
             </div>
-            <div className="mt-4 grid grid-cols-1 gap-1 border border-gray-200 bg-white-200 sm:grid-cols-2 lg:grid-cols-3">
+            <div className="mt-4 grid grid-cols-1 gap-1 border-gray-200 bg-white-200 sm:grid-cols-2 lg:grid-cols-4">
               {products.map((item) => (
                 <a
                   href="#"
-                  className="block bg-white"
+                  className="block bg-slate-300"
                   key={item.product_id + item.category.id}
                 >
                   <img
